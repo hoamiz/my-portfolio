35623b51e1fff0cfc759624fce123c0efe640adb
son.le <leson11081997@gmail.com>
2025-11-11 00:31:56 +0700

    remove key

--- app/api/sendEmail/route.tsx
+++ app/api/sendEmail/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import EmailTemplate from "@/components/EmailTemplate";
 
-const resend = new Resend(process?.env?.RESEND_API_KEY || "re_3KG5riJu_8f5SBQBWD3afDe7uHzajquAU");
+const resend = new Resend(process?.env?.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
